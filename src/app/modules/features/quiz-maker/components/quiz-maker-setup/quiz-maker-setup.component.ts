ab3a211db377e120100337fb1146f8fde5ae3f7b
Valentin LORIQUET <valentin.loriquet@soprasteria.com>
2023-08-23 17:44:18 +0200

    more config for stackblitz

--- src/app/modules/features/quiz-maker/components/quiz-maker-setup/quiz-maker-setup.component.ts
+++ src/app/modules/features/quiz-maker/components/quiz-maker-setup/quiz-maker-setup.component.ts
@@ -1,26 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Category } from 'src/app/models/category';
-import { Difficulties } from 'src/app/models/enums/difficulties.enum';
-import { TriviaDataService } from 'src/app/services/trivia-data.service';
+import { Category } from '../../../../../models/category';
+import { Difficulties } from '../../../../../models/enums/difficulties.enum';
+import { TriviaDataService } from '../../../../../services/trivia-data.service';
 
 @Component({
-  selector: 'app-quiz-maker-setup',
-  templateUrl: './quiz-maker-setup.component.html',
-  styleUrls: ['./quiz-maker-setup.component.scss'],
+    selector: 'app-quiz-maker-setup',
+    templateUrl: './quiz-maker-setup.component.html',
+    styleUrls: ['./quiz-maker-setup.component.scss']
 })
 export class QuizMakerSetupComponent implements OnInit {
-  categories$: Observable<Category[]> = this.triviaDataService.getCategories();
-  difficulties = Object.values(Difficulties);
+    categories$: Observable<Category[]> = this.triviaDataService.getCategories();
+    difficulties = Object.values(Difficulties);
 
-  selectedCategory: Category | undefined;
-  selectedDifficulty: Difficulties | undefined;
+    selectedCategory: Category | undefined;
+    selectedDifficulty: Difficulties | undefined;
 
-  constructor(private triviaDataService: TriviaDataService) {}
+    constructor(private triviaDataService: TriviaDataService) {}
 
-  ngOnInit(): void {
-    this.triviaDataService
-      .getCategories()
-      .subscribe((data) => console.log(data));
-  }
+    ngOnInit(): void {
+        this.triviaDataService.getCategories().subscribe((data) => console.log(data));
+    }
 }
